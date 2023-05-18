ea134bb9d057bbb6cd1c8c7cb66655dba40e601e
azamn <azam.noor@techswevil.com>
2023-05-18 09:08:02 +0500

    initial commit on working branch

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'task6 flutter flavours',
+      title: 'Task6 flutter flavours',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
