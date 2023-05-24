7c66da78baefec3fb3c13caafb57812da4e0ab98
azamn <azam.noor@techswevil.com>
2023-05-24 20:12:26 +0500

    task 6 done

--- lib/helper/stringResources.dart
+++ lib/helper/stringResources.dart
@@ -1,5 +1,6 @@
 class StringResources {
   static String TITLE = 'task6 flutter flavours';
   static String CRASH_BTN = 'Crash app';
-  static String TOKEN_BTN = "token";
+  static String TOKEN_BTN = "Get Remote Value";
+  static double twenty = 20.0;
 }
